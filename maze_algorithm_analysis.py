1bd80626dc582e1fc22ebacda8851ff88501dd80
jolenewilliams82301 <jolenewilliams450@gmail.com>
2025-11-25 09:42:27 -0800

    make data visualization more readable

--- maze_algorithm_analysis.py
+++ maze_algorithm_analysis.py
@@ -19,6 +19,8 @@ from A_star_algorithm import a_star_find_path
 from DFS_algorithm import dfs_find_path
 
 
+'''Helper Functions'''
+
 def collect_data(algorithm_titles:list, path_lengths:list, execution_times:list):
     '''Collect data for data analysis/ visualization'''
 
@@ -44,13 +46,19 @@ def visualize_maze(maze_array, path, col, alg, title, axes):
     axes.plot( path_item_vals, path_row_vals, color = col, lw = 1, label=alg )
 
     # other settings
+    axes.set_xticks([])
+    axes.set_yticks([])
     axes.legend()
-    axes.minorticks_off()
-    
+
+'''Main Function'''
+
 def maze_algorithm_analysis():
+    '''Main function of the program. Generate a maze, solve using three algorithms, 
+    show the mazes and each algorithm's solution, then visualize the performance data'''
 
     # Generate the maze
     maze_array, start, goal = generate_maze(20,50)
+    maze_id = 0
 
     # Generate each algorithm's solution to the maze
     a_et_start = perf_counter()
@@ -61,23 +69,32 @@ def maze_algorithm_analysis():
     dfs_path = dfs_find_path(maze_array, start, goal)
     dfs_et_end = perf_counter()
     
+    '''Visualization'''
+
     # Visualize the maze along with each algorithm's solution
     fig1, (astar_ax, dfs_ax) = plt.subplots(nrows=2,ncols=1, figsize=(10,10))
     fig1.suptitle('Mazes solved by each algorithm')
     visualize_maze(maze_array, astar_path, 'red', 'A* Algorithm', 'A* Algorithm', astar_ax)
     visualize_maze(maze_array, dfs_path, 'blue', 'DFS Algorithm', 'DFS Algorithm', dfs_ax)
-    
-    # Data Analysis
+    fig1.tight_layout()
 
-
-    # Data for node visitations
+    # Get performance data in dataframe
     df = collect_data(['A* Algorithm', 'DFS Algorithm'],[len(astar_path), len(dfs_path)], [(a_et_end - a_et_start), (dfs_et_end - dfs_et_start)] )
 
+    # Create bar graphs 
+    fig2, (node_visitations_bargraph, execution_times_bargraph) = plt.subplots(nrows=2, ncols=1)
+    fig2.suptitle('Performance Data')
 
-    fig2, (bargraph1, bargraph2) = plt.subplots(nrows=2, ncols=1)
-    sns.set_theme(color_codes=True)
-    sns.barplot(ax=bargraph1,x='algorithm_title', y='node_visitations', data=df)
-    sns.barplot(ax=bargraph2, x='algorithm_title', y = 'execution_time',data=df)
+    # Bar graph for total node visitations per algorithm
+    sns.barplot(ax=node_visitations_bargraph,y='algorithm_title', x='node_visitations', hue='algorithm_title', data=df, width=0.4)
+    node_visitations_bargraph.set_ylabel("", fontsize=10)
+    node_visitations_bargraph.set_xlabel("Node Visitations", fontsize=10)
+
+    # Bar graph for execution time per algorithm
+    sns.barplot(ax=execution_times_bargraph, y='algorithm_title', x = 'execution_time',hue='algorithm_title',data=df, width=0.4)
+    execution_times_bargraph.set_ylabel("", fontsize=10)
+    execution_times_bargraph.set_xlabel("Execution Time (seconds)", fontsize=10)
+    fig2.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
