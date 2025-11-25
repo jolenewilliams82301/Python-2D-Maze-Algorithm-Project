b4fbc673737e07aca0260cac739fbfa344f14a5d
jolenewilliams82301 <jolenewilliams450@gmail.com>
2025-11-25 00:37:22 -0800

    remove unnecessary imports from A* algorithm

--- A_star_algorithm.py
+++ A_star_algorithm.py
@@ -5,12 +5,6 @@ Notes:
 x = row, y = item
 
 '''
-import math
-import numpy as np
-import random as rn
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-import sys
 from maze_generation import generate_maze
 import heapq
 
