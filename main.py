a9f41335a2395827a9fdc1cb1773ce61034af5e4
jolenewilliams82301 <jolenewilliams450@gmail.com>
2025-11-25 00:24:22 -0800

    adding to main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 '''Starting point for program. tkinter menu to prompt user to generate a new maze or exit program'''
 import tkinter
+from maze_algorithm_analysis import maze_algorithm_analysis
+
+
+
+
+if __name__ == "__main__":
+    maze_algorithm_analysis()
